d1c6dfb495c8b252ae98ac7977e6d328905d4504
devashish700 <111717473+devashish700@users.noreply.github.com>
2024-02-05 17:46:52 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+   /* <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
@@ -18,6 +18,7 @@ export default function Home() {
       </main>
 
       <Footer />
-    </div>
+    </div>*/
+    
   )
 }
